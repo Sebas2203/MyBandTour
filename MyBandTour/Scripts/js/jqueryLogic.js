6d4986ad39c36b1be09649efe52087da01a34115
Sebastian Mora <133081239+Sebas2203@users.noreply.github.com>
2025-07-20 18:34:32 -0600

    advance

--- MyBandTour/Scripts/js/jqueryLogic.js
+++ MyBandTour/Scripts/js/jqueryLogic.js
@@ -1,1 +1,18 @@
-﻿
+﻿function Authenticate() {
+    //parametros de entrada
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+
+
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        URL: "/Home/AuthenticateUser",
+        data: { parametro: valor },
+        success: function (response) {
+        },
+        Error: function (error) {
+            console.log("Error: " + error);
+        }
+    }
+}
